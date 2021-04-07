2f9c0d11048b3ad8f97a465d554947dc3e740acf
Urgyen Shakya <urgyenshakya@gmail.com>
2021-04-07 08:51:45 +0545

    day1

---
+++ src/Day1.java
@@ -0,0 +1,122 @@
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Basic Java;
+		//VAriables;
+		//Data type
+		
+		//int age = 25;
+		
+		
+		
+		//Naming Convention;
+		//1. VAriable, Small letter 
+		//first name --> Lower Camel Casing??? 
+//		mynameisapplebanana
+//		myNameIsAppleBanana;  -->> Lower CAmel CAsing
+//		
+		//Datatypes
+		
+		//2 types of datatype??
+		//Primitive
+			//aru koi sanga dependent
+				//int age = 25;
+		
+		
+		//Non Primitive;;
+			//dependenet
+				//String name ="Urgyen";
+				//String - char('U') +char('r')
+				
+	//	boolean isActive =0 or 1;
+		
+		//float 
+		
+		
+		//long
+		//double valueOfPi= 50;  //8 byte
+		
+		char class1= 'a';      
+	//	System.out.println(class1);
+		//ASCII Value    //Task1
+		
+		
+		//Operators?
+		
+		//1. 	Arithemetic Operators;
+		//    	% modulo
+		//		System.out.println(6%2);
+		
+		//2. Unary Opertors
+		
+//		int number =10;
+////		//PostFix
+////		
+////		//Assignment Operators;
+////		number*=5;  //number= number+2;
+////		number+=5;
+////		
+////		//Prefix
+//		--number;
+//		--number;
+////		
+//		System.out.println(number);
+
+		//Shift Opertaors << , >> 
+		
+		//Relational Operators
+		
+		//Logical Operators
+		
+		//Logic Gates???
+		
+		//And  //
+		
+//		int age= 20;
+//		
+//		// true or false  -> true
+//		if(  age>10  ||   age==10 ) {
+//			System.out.println("OK");
+//		}
+		
+//		t and t --> 	t
+//		t and f -- > 	f
+//		f and t  --> 	f
+//		f and f  --> 	f
+		
+		//Or
+		
+		//t or t --> t
+		//t or f --> t
+		//f or t --> t
+		//f or f --> f 
+		
+		//Ternary Operator??
+		
+		//Simplified If else 
+		
+	
+	int age=100;
+	
+	//(Condition )? assign this If true : assign this if False;
+	
+	String ageGroup = age<20 ? "asdasdasf" :	"Teen + Child" ;
+	
+	System.out.println(ageGroup);
+
+		//Control Statements	
+			//if
+			//if else
+			//else if
+			//switch
+		//Loops
+			//While
+			//Do while
+			//For 	
+			
+	}
+
+}
