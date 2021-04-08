ada4de53607ecc522b5f47748a163d5f46a03524
Urgyen Shakya <urgyenshakya@gmail.com>
2021-04-08 08:45:10 +0545

    day 2

---
+++ src/day2/Day2.java
@@ -0,0 +1,138 @@
+package day2;
+
+public class Day2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Control Statements??
+//Error
+		// Buisness Logic
+		//int eletricity_unit = -1;
+
+		// if condition only result for true
+		// False
+
+//		if ( eletricity_unit < 101) {
+//			double amount = eletricity_unit * 5;
+//			System.out.println("Schema A Electricity Bill: " + amount);
+//		} 
+//		
+//		else if ( eletricity_unit < 201) {
+//			double amount = eletricity_unit * 10;
+//			System.out.println("Scheme B Electricity Bill: " + amount);
+//		} 
+//		
+//		else if ( eletricity_unit < 301) {
+//			double amount = eletricity_unit * 20;
+//			System.out.println("Schema CElectricity Bill: " + amount);
+//		}
+//		
+//		else {
+//			double amount = eletricity_unit * 30;
+//			System.out.println(" Scheme D Electricity Bill: " + amount);
+//		}
+
+		// 0-100 per unit 5 //ok
+
+		// 101-200 per unit 10
+
+		// 200-300 per unit 20
+
+		// 300+ per unit 30
+
+		// Switch
+		// weekday =7 Day ?? SAturday
+		// weekday =2 Day Monday
+
+//		int weekDay = 9;
+//
+//		switch (weekDay) {
+//
+//		case 1:
+//			System.out.println("Sunday");
+//			break;
+//
+//		case 2:
+//			System.out.println("Monday");
+//			break;
+//
+//			
+//		case 3:
+//			System.out.println("Tuesday");
+//			break;
+//
+//			
+//		case 4:
+//			System.out.println("Wednesday");
+//			break;
+//
+//			
+//		case 5:
+//			System.out.println("Thursday");
+//			break;
+//
+//			
+//		case 6:
+//			System.out.println("Friday");
+//			break;
+//
+//			
+//		case 7:
+//			System.out.println("SAturday");
+//			break;
+//			
+//		default:
+//			System.out.println();
+//
+//			
+//
+//		}
+
+		//Loops
+		//1. while
+		//2. Do while
+		//3. For Loop
+		
+		
+//		//Index Initialization;
+//		int times=-1;
+//		
+//		//Condition Check
+//		while(times<10) {
+//			
+//			System.out.println(times+ " Mero Nammm");
+//			//Increment/ Decrement
+//			times++;
+//			
+//		}
+		
+		
+		//do while//1-100
+		
+		//  5 * 1 = 5
+		//5 *2 =10
+//		int times=5;
+//		int count =1;
+//		
+//		do {
+//			
+//			System.out.println(times  + " * " + count + " = "+ times*count );
+//			//times+=2; //
+//		count++;
+//			
+//		}while(count<=12);
+//		
+		
+		//even 
+		for(int index=2 ; index>100 ; index=index+2) {
+			
+			System.out.println(index);
+	
+		}
+		
+		
+		
+	}
+
+}
